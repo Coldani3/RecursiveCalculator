f024d557037ce676913f83ce0d20a6552f452814
Coldani3 <p110117069@gmail.com>
2021-02-25 20:25:43 +0000

    More work, initial draft of calculator is done and pending testing. TODO: Harvard Reference the borrowed code.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace RecursiveCalculator
 {
@@ -16,6 +17,7 @@ namespace RecursiveCalculator
 	6*2/4 = 3
 	(3+2)/4 = 1.25
 	3+(2/4) = 3.5
+	6+(12/2)-9+(6*9)+4=61
 	*/
     class Program
     {
@@ -23,16 +25,24 @@ namespace RecursiveCalculator
         {
             Console.WriteLine("Enter equation:");
 			string equation = Console.ReadLine();
-			Console.WriteLine($"Result: {Calculate(equation)}");
+			try
+			{
+				float result = Calculate(equation);
+				Console.WriteLine($"Result: {result}");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
 
-
+			Console.ReadKey(true);
         }
 
 		static float Calculate(string toCalc)
 		{
 			string theCalc = toCalc;
 			float total = 0;
-			//find brackets
+			//first, find brackets, calculate them and substitute them into the string.
 			for (int i = 0; i < toCalc.Length; i++)
 			{
 				if (toCalc[i] == '(')
@@ -40,11 +50,65 @@ namespace RecursiveCalculator
 					int endBracketIndex = IndexOfMatchingBracket(toCalc, i);
 					if (endBracketIndex == -1) throw new FormatException("Open bracket is missing a closed bracket!");
 					string bracketCalcString = toCalc.Substring(i, endBracketIndex - i);
-					total += Calculate(toCalc.Substring(i, IndexOfMatchingBracket(toCalc, i) - i));
+					//RECURSION TIME
+					float bracketResult = Calculate(bracketCalcString);
+					ReplaceFirstOccurrence(theCalc, bracketCalcString, bracketResult.ToString());
+					//theCalc.Replace(bracketCalcString, bracketResult.ToString());
+				}
+			}
+
+			//with all the brackets removed, it is now safe to treat it as if there are no brackets. we use 'theCalc' from here.
+			char[] operators = new char[] {'^', '/', '*', '+', '-'};
+
+			//loop through the calc by order of BIDMAS, and replace found calcs with the result of said calc
+			//this should eventually mean the calc will be reduced to the result.
+
+			foreach (char currOperator in operators)
+			{
+				for (int i = 0; i < theCalc.Length; i++)
+				{
+					if (theCalc[i] == currOperator)
+					{
+						int? prevNumStartIndex = null;
+						int? postNumStartIndex = null;
+
+						for (int i2 = i; i2 >= 0; i2--)
+						{
+							if (!IsNumberOrDecimalPlace(theCalc[i2])) 
+							{
+								prevNumStartIndex = i2 + 1;
+								break;
+							}
+						}
+						//if an index was not found, assume it's at the very start of theCalc
+						if (prevNumStartIndex == null) prevNumStartIndex = 0;
+
+						for (int i2 = i; i2 < theCalc.Length; i2++)
+						{
+							if (!IsNumberOrDecimalPlace(theCalc[i2])) 
+							{
+								postNumStartIndex = i2 - 1;
+								break;
+							}
+						}
+
+						if (postNumStartIndex == null) postNumStartIndex = theCalc.Length - 1;
+
+						string splitCalc = theCalc.Substring((int) prevNumStartIndex, (int) (postNumStartIndex - prevNumStartIndex));
+						float result = SingleOperationCalculate(splitCalc);
+						theCalc = ReplaceFirstOccurrence(theCalc, splitCalc, result.ToString());
+						//set i to be at the start of the result string
+						//i is the index of the operator in the middle of the split out calc
+						//set i to the index of the start. we'll go over the result of this loop but it won't do anything
+						//so it's slightly inefficient but more reliable.
+						i = (int) prevNumStartIndex;
+						//i += result.ToString().Length - 1;
+						//i -= splitCalc.Length - result.ToString().Length;
+					}
 				}
 			}
 			
-
+			return float.Parse(theCalc);
 		}
 
 		//calculate a simple two number one operation calc, e.g 2+2
@@ -52,7 +116,7 @@ namespace RecursiveCalculator
 		{
 			foreach (char character in toCalc)
 			{
-				if (!IsNumber(character) && character != '.')
+				if (!IsNumberOrDecimalPlace(character))
 				{
 					//it is an operation
 					float result;
@@ -111,12 +175,9 @@ namespace RecursiveCalculator
 				for (int i = openingIndex + 1; i < calc.Length; i++)
 				{
 					if (calc[i] == '(') depth++;
-					else if (calc[i] == ')') 
-					{
-						depth--;
+					else if (calc[i] == ')') depth--;
 
-						if (depth == 0) return i;
-					}
+					if (depth == 0) return i;
 				}
 			}
 
@@ -127,17 +188,25 @@ namespace RecursiveCalculator
 		{
 			foreach (char character in toCheck)
 			{
-				if (!IsNumber(character) && character != '.') return false;
+				if (!IsNumberOrDecimalPlace(character) && character != '.') return false;
 			}
 
 			return true;
 		}
 
-		static bool IsNumber(char toCheck)
+		static bool IsNumberOrDecimalPlace(char toCheck)
 		{
 			int asciiCode = (int) toCheck;
-			if (!(asciiCode >= 48 && asciiCode <= 57)) return false;
+			if (!((asciiCode >= 48 && asciiCode <= 57) || toCheck == '.')) return false;
 			return true;
+		}
+
+		//from https://stackoverflow.com/a/146747, referenced 25/02/2021
+		static string ReplaceFirstOccurrence(string originalString, string toReplace, string replaceWith)
+		{
+			Regex regex = new Regex("foo");
+			string result = regex.Replace("foo1 foo2 foo3 foo4", "bar", 1);  
+			return result;
 		}
     }
 }
