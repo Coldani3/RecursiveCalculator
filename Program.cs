a4fe80221768e409da57071de13734f14b221077
Coldani3 <p110117069@gmail.com>
2021-02-28 21:03:10 +0000

    Now works for all non bracket calculations. Still working on the brackets. Notably fixed this by going with the 'order brackets' approach of BIDMAS (ie. plus and minus should be done left to right at the same time, instead of pluses first and then minuses after).

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ namespace RecursiveCalculator
 		static readonly bool DebugMode = true;
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter equation:");
+            Console.WriteLine("Enter equation (please put negative numbers in brackets):");
 			string equation = Console.ReadLine().Replace(" ", "");
 			try
 			{
@@ -43,18 +43,25 @@ namespace RecursiveCalculator
 
 		static float Calculate(string toCalc)
 		{
-			Debug("DEBUG: Begin Calculate");
+			Debug("DEBUG: Calculate() Begin Calculate");
 			string theCalc = toCalc;
+
+			//first, check to see if the first symbol is a minus and put 0 in front of it
+			if (toCalc[0] == '-')
+			{
+				theCalc = '0' + theCalc;
+			}
 
 			//then, find brackets, calculate them and substitute them into the string.
 			for (int i = 0; i < toCalc.Length; i++)
 			{
 				if (toCalc[i] == '(')
 				{
-					Debug("Found bracket!");
+					Debug("DEBUG: Calculate() Found bracket!");
 					int endBracketIndex = IndexOfMatchingBracket(toCalc, i);
 					if (endBracketIndex == -1) throw new FormatException("Open bracket is missing a closed bracket!");
-					string bracketCalcString = toCalc.Substring(i, endBracketIndex - i + 1);
+					Debug($"DEBUG: Calculate() toCalc[endBracketIndex] {toCalc[endBracketIndex]}");
+					string bracketCalcString = toCalc.Substring(i + 1, endBracketIndex - i);
 					//RECURSION TIME
 					float bracketResult = Calculate(bracketCalcString);
 					ReplaceFirstOccurrence(theCalc, bracketCalcString, bracketResult.ToString());
@@ -62,51 +69,55 @@ namespace RecursiveCalculator
 				}
 			}
 
+			char[][] operatorss = new char[][] {new char[] {'^'}, new char[] {'/', '*'}, new char[] {'+', '-'}};
+
 			//with all the brackets removed, it is now safe to treat it as if there are no brackets. we use 'theCalc' from here.
 			char[] operators = new char[] {'^', '/', '*', '+', '-'};
 
 			//loop through the calc by order of BIDMAS, and replace found calcs with the result of said calc
 			//this should eventually mean the calc will be reduced to the result.
 
-			foreach (char currOperator in operators)
+			foreach (char[] currOperators in operatorss)
 			{
 				Func<int> len = () => theCalc.Length;
 				for (int i = 0; i < len(); i++)
 				{
-					if (theCalc[i] == currOperator)
+					if (ArrayContains(currOperators, theCalc[i]))
 					{
 						int? prevNumStartIndex = null;
 						int? postNumStartIndex = null;
 
-						for (int i2 = i; i2 >= 0; i2--)
+						for (int i2 = i - 1; i2 >= 0; i2--)
 						{
 							if (!IsNumberOrDecimalPlace(theCalc[i2])) 
 							{
-								prevNumStartIndex = i2 - 1;
+								prevNumStartIndex = i2 + 1;
 								break;
 							}
 						}
 						//if an index was not found, assume it's at the very start of theCalc
 						if (prevNumStartIndex == null) prevNumStartIndex = 0;
 
-						for (int i2 = i; i2 < theCalc.Length; i2++)
+						for (int i2 = i + 1; i2 < theCalc.Length; i2++)
 						{
 							if (!IsNumberOrDecimalPlace(theCalc[i2])) 
 							{
-								postNumStartIndex = i2 + 1;
+								postNumStartIndex = i2 - 1;
 								break;
 							}
 						}
 
 						if (postNumStartIndex == null) postNumStartIndex = theCalc.Length - 1;
 
-						Debug($"DEBUG: prevNumSI: {prevNumStartIndex}, postNumSI: {postNumStartIndex}, i: {i}");
+						Debug($"DEBUG: Calculate() prevNumSI: {prevNumStartIndex}, postNumSI: {postNumStartIndex}, i: {i}");
 						string splitCalc = theCalc.Substring((int) prevNumStartIndex, (int) (postNumStartIndex - prevNumStartIndex + 1));
-						Debug($"DEBUG: splitCalc: {splitCalc}");
+						Debug($"DEBUG: Calculate() splitCalc: {splitCalc}");
 						float result = SingleOperationCalculate(splitCalc);
-						Debug($"DEBUG: pre replace theCalc: {theCalc}, splitCalc: {splitCalc}, result: {result}, result.ToString(): {result.ToString()}");
-						theCalc = ReplaceFirstOccurrence(theCalc, splitCalc, result.ToString());
-						Debug($"DEBUG: Replaced calc: {theCalc}");
+						string strResult = result.ToString();
+						if (strResult[0] == '-') strResult = '0' + strResult;
+						Debug($"DEBUG: Calculate() pre replace theCalc: {theCalc}, splitCalc: {splitCalc}, result: {result}, result.ToString(): {result.ToString()}");
+						theCalc = ReplaceFirstOccurrence(theCalc, splitCalc, strResult);
+						Debug($"DEBUG: Calculate() Replaced calc: {theCalc}");
 						//set i to be at the start of the result string
 						//i is the index of the operator in the middle of the split out calc
 						//set i to the index of the start. we'll go over the result of this loop but it won't do anything
@@ -118,14 +129,14 @@ namespace RecursiveCalculator
 				}
 			}
 			
-			Debug($"DEBUG: theCalc: {theCalc}");
+			Debug($"DEBUG: Calculate() theCalc: {theCalc}");
 			return float.Parse(theCalc);
 		}
 
 		//calculate a simple two number one operation calc, e.g 2+2
 		static float SingleOperationCalculate(string toCalc)
 		{
-			Debug("DEBUG: Calc: " + toCalc);
+			Debug("DEBUG: SingleOperationCalculate() Calc: " + toCalc);
 			foreach (char character in toCalc)
 			{
 				if (!IsNumberOrDecimalPlace(character))
@@ -140,35 +151,35 @@ namespace RecursiveCalculator
 					{
 						case '+':
 							splitCalc = toCalc.Split('+');
-							Debug($"DEBUG: splitCalc 0: {splitCalc[0]}, 1: {splitCalc[1]}");
+							Debug($"DEBUG: SingleOperationCalculate() splitCalc 0: {splitCalc[0]}, 1: {splitCalc[1]}");
 							num1 = float.Parse(splitCalc[0]);
 							num2 = float.Parse(splitCalc[1]);
 							result = num1 + num2;
 							break;
 						case '-':
 							splitCalc = toCalc.Split('-');
-							Debug($"DEBUG: splitCalc 0: {splitCalc[0]}, 1: {splitCalc[1]}");
+							Debug($"DEBUG: SingleOperationCalculate() splitCalc 0: {splitCalc[0]}, 1: {splitCalc[1]}");
 							num1 = float.Parse(splitCalc[0]);
 							num2 = float.Parse(splitCalc[1]);
 							result = num1 - num2;
 							break;
 						case '*':
 							splitCalc = toCalc.Split('*');
-							Debug($"DEBUG: splitCalc 0: {splitCalc[0]}, 1: {splitCalc[1]}");
+							Debug($"DEBUG: SingleOperationCalculate() splitCalc 0: {splitCalc[0]}, 1: {splitCalc[1]}");
 							num1 = float.Parse(splitCalc[0]);
 							num2 = float.Parse(splitCalc[1]);
 							result = num1 * num2;
 							break;
 						case '/':
 							splitCalc = toCalc.Split('/');
-							Debug($"DEBUG: splitCalc 0: {splitCalc[0]}, 1: {splitCalc[1]}");
+							Debug($"DEBUG: SingleOperationCalculate() splitCalc 0: {splitCalc[0]}, 1: {splitCalc[1]}");
 							num1 = float.Parse(splitCalc[0]);
 							num2 = float.Parse(splitCalc[1]);
 							result = num1 / num2;
 							break;
 						case '^':
 							splitCalc = toCalc.Split('^');
-							Debug($"DEBUG: splitCalc 0: {splitCalc[0]}, 1: {splitCalc[1]}");
+							Debug($"DEBUG: SingleOperationCalculate() splitCalc 0: {splitCalc[0]}, 1: {splitCalc[1]}");
 							num1 = float.Parse(splitCalc[0]);
 							num2 = float.Parse(splitCalc[1]);
 							result = (float) Math.Pow(num1, num2);
@@ -180,6 +191,7 @@ namespace RecursiveCalculator
 					return result;
 				}
 			}
+
 			//the entire calculation is a number, so we can just return that.
 			return float.Parse(toCalc);
 		}
@@ -218,7 +230,6 @@ namespace RecursiveCalculator
 			return true;
 		}
 
-		//from https://stackoverflow.com/a/146747, referenced 25/02/2021
 		//from https://stackoverflow.com/a/141076, referenced 28/02/2021
 		static string ReplaceFirstOccurrence(string originalString, string toReplace, string replaceWith)
 		{
@@ -228,6 +239,16 @@ namespace RecursiveCalculator
 				return originalString;
 			}
 			return originalString.Substring(0, pos) + replaceWith + originalString.Substring(pos + toReplace.Length);  
+		}
+
+		static bool ArrayContains<T>(T[] array, T contains)
+		{
+			foreach (T obj in array)
+			{
+				if (obj.Equals(contains)) return true;
+			}
+
+			return false;
 		}
 
 		static void Debug(string message)
